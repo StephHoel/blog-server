d1514088f9201a5da596c42dfc987670665e04a5
Steph Hoel <steph.hoel@gmail.com>
2023-11-16 12:30:24 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Servidor para o site "Steph Hoel Blog"
+
+## Post
+
+| Método | Caminho           |
+| ------ | ----------------- |
+| Get    | `/post`           |
+| Get    | `/post/:idPost`   |
+| Post   | `/post`           |
+| Put    | `/post`           |
+| Delete | `/post/:idPost`   |
+
+## Login
+
+| Método | Caminho           |
+| ------ | ----------------- |
+| Post   | `/register`       |
+| Post   | `/login`          |
+| Put    | `/login`          |
+| Get    | `/login/:idLogin` |
+| Delete | `/login`          |
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import cors from '@fastify/cors'
+import Fastify from 'fastify'
+
+import loginRoutes from './routes/loginRoutes'
+import postRoutes from './routes/postRoutes'
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true,
+    // só permite requisições vindas do site abaixo, qualquer outra deve ser ignorada
+    // origin: 'https://stephhoel.github.io/lista-de-compras/',
+  })
+
+  await fastify.register(postRoutes)
+  await fastify.register(loginRoutes)
+
+  await fastify.listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+}
+
+bootstrap()
